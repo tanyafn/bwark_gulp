f2f62f07bb7c074e8b25eafae5e3057f79b66e9e
Tatyana Fonina <tanyafn@gmail.com>
2016-05-12 15:04:50 +1000

    Lint javascript with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
-var gulp   = require('gulp');
+var gulp   = require('gulp'),
+    eslint = require('gulp-eslint');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
-gulp.task('scripts', function(){
+gulp.task('lint', function() {
+  return gulp.src('./src/*.js')
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+ });
+
+gulp.task('scripts', function() {
   return gulp.src("./src/*.js")
     .pipe(concat('bundle.js'))
     .pipe(uglify())
     .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('default', ['scripts']);
+gulp.task('default', ['lint', 'scripts']);
