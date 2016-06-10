7714b785494d3d74eb110592d3e806bd6ba79a4e
Andrei Gridnev <andrew.gridnev@gmail.com>
2016-06-10 23:07:49 +1000

    Add Jade templates

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,47 @@
-var gulp   = require('gulp'),
-    eslint = require('gulp-eslint');
+var gulp   = require('gulp');
+var eslint = require('gulp-eslint');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
+var jade = require('gulp-jade');
+
+var paths = {
+  js: './src/js/*.js',
+  sass: './src/scss/*.{scss,sass}',
+  jade: 'src/templates/*.jade',
+  tmp: './tmp',
+  index: './tmp/index.html',
+  dist: './dist'
+}
 
 gulp.task('lint', function() {
-  return gulp.src('./src/*.js')
+  return gulp.src(paths.js)
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
  });
 
 gulp.task('scripts', function() {
-  return gulp.src("./src/js/*.js")
+  return gulp.src(paths.js)
     .pipe(concat('bundle.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest(paths.dist));
 });
 
 gulp.task('sass', function () {
-  gulp.src('./src/scss/**/*.{scss,sass}')
+  gulp.src(paths.sass)
     .pipe(sass({ outputStyle: 'compressed' }))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest(paths.dist));
 })
 
-gulp.task('default', ['lint', 'scripts', 'sass']);
+gulp.task('jade', function() {
+  return gulp.src(paths.jade)
+    .pipe(jade({pretty: true}))
+    .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('default', ['lint', 'scripts', 'sass', 'jade']);
 
 gulp.task('watch', function() {
-  gulp.watch('./src/**/*.{scss,sass,js}', ['sass', 'scripts'])
+  gulp.watch('./src/**/*.{scss,sass,js,jade}', ['sass', 'scripts', 'jade'])
 })
