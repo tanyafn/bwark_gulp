55d2ece0fcb49a3d6f74de3fdd45d9ff1bf5f86f
Andrei Gridnev <andrew.gridnev@gmail.com>
2016-06-11 10:09:17 +1000

    Sdd gulp serve and jade

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,15 @@ var gulp   = require('gulp');
 var eslint = require('gulp-eslint');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var sass = require('gulp-sass');
-var jade = require('gulp-jade');
+var sass   = require('gulp-sass');
+var jade   = require('gulp-jade');
+var gls    = require('gulp-live-server');
 
 var paths = {
-  js: './src/js/*.js',
-  sass: './src/scss/*.{scss,sass}',
-  jade: 'src/templates/*.jade',
-  tmp: './tmp',
-  index: './tmp/index.html',
-  dist: './dist'
+  js:    './src/js/*.js',
+  sass:  './src/scss/*.{scss,sass}',
+  jade:  ['src/templates/**/*.jade', '!src/templates/partials/*', '!src/templates/layout.jade'],
+  build: './build'
 }
 
 gulp.task('lint', function() {
@@ -19,28 +18,37 @@ gulp.task('lint', function() {
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
- });
+});
 
 gulp.task('scripts', function() {
   return gulp.src(paths.js)
     .pipe(concat('bundle.js'))
     .pipe(uglify())
-    .pipe(gulp.dest(paths.dist));
+    .pipe(gulp.dest(paths.build));
 });
 
 gulp.task('sass', function () {
-  gulp.src(paths.sass)
+  return gulp.src(paths.sass)
     .pipe(sass({ outputStyle: 'compressed' }))
-    .pipe(gulp.dest(paths.dist));
+    .pipe(gulp.dest(paths.build));
 })
 
 gulp.task('jade', function() {
   return gulp.src(paths.jade)
-    .pipe(jade({pretty: true}))
-    .pipe(gulp.dest(paths.dist));
+    .pipe(jade({ pretty: true }))
+    .pipe(gulp.dest(paths.build));
 });
 
-gulp.task('default', ['lint', 'scripts', 'sass', 'jade']);
+gulp.task('default', ['lint',  'jade', 'scripts', 'sass']);
+
+gulp.task('serve', function() {
+  var server = gls.static(paths.build, 3000);
+  server.start();
+
+  gulp.watch(['build/*.html', 'build/*.css'], function (file) {
+    server.notify.apply(server, [file]);
+  });
+});
 
 gulp.task('watch', function() {
   gulp.watch('./src/**/*.{scss,sass,js,jade}', ['sass', 'scripts', 'jade'])
